738e8d635598d69285ab31bd1461dc1563976824
jobum97 <brian654321@naver.com>
2022-07-05 12:46:47 +0900

    db 연결 삽질중

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SharedModule } from './shared/shared.module';
+import { ApiConfigService } from './shared/services/api-config.service';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    //UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [SharedModule],
+      useFactory: (configService: ApiConfigService) =>
+        configService.postgresConfig,
+      inject: [ApiConfigService],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 declare const module: any;
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import { ApiConfigService } from './shared/services/api-config.service';
+//import { TranslationService } from './shared/services/translation.service';
+import { SharedModule } from './shared/shared.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
 
+  app.use(helmet());
+  // app.setGlobalPrefix('/api'); use api as global prefix if you don't have subdomain
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  const configService = app.select(SharedModule).get(ApiConfigService);
+
+  const port = configService.appConfig.port;
+  await app.listen(port);
+
+  console.info(`server running on ${await app.getUrl()}`);
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
