a8e155e9d618bbb1e7d525797feef8ea350708b0
jobum97 <brian654321@naver.com>
2022-07-05 14:46:33 +0900

    [#1] db 연결 초안 일단되긴함

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SharedModule } from './shared/shared.module';
-import { ApiConfigService } from './shared/services/api-config.service';
-import { UserModule } from './modules/user/user.module';
+import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    //UserModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [SharedModule],
-      useFactory: (configService: ApiConfigService) =>
-        configService.postgresConfig,
-      inject: [ApiConfigService],
-    }),
-    UserModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), AuthModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-declare const module: any;
-import rateLimit from 'express-rate-limit';
-import helmet from 'helmet';
-import { ApiConfigService } from './shared/services/api-config.service';
-//import { TranslationService } from './shared/services/translation.service';
-import { SharedModule } from './shared/shared.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet());
-  // app.setGlobalPrefix('/api'); use api as global prefix if you don't have subdomain
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-    }),
-  );
-  const configService = app.select(SharedModule).get(ApiConfigService);
+  const serverConfig = config.get('server');
+  const port = serverConfig.port;
 
-  const port = configService.appConfig.port;
   await app.listen(port);
-
-  console.info(`server running on ${await app.getUrl()}`);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+  Logger.log(`Application running on port ${port}`);
 }
 bootstrap();
