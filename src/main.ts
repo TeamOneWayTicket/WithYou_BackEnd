a6eed3cd21bb05d34bfc85b93215fc7b72f5210c
jobum97 <brian654321@naver.com>
2022-07-19 16:54:44 +0900

    [#9]feat: create swagger api-docs

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SharedModule } from './shared/shared.module';
 import { ApiConfigService } from './shared/services/api-config.service';
+import { setupSwagger } from './utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.select(SharedModule).get(ApiConfigService);
   const port = configService.appConfig.port;
   console.log('server run on port #', port);
+  setupSwagger(app);
   await app.listen(port);
 }
 bootstrap();
