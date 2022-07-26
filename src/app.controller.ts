91f9bbe7715ad7314e73e0548d63e3d98e725ff4
jobum97 <brian654321@naver.com>
2022-07-26 13:59:49 +0900

    [#13]feat: 메뉴추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,12 +17,20 @@ export class AppController {
       <div>
         <h1>WithYou Dev Menu</h1>
 
+        <form action="/api-docs" method="GET">
+          <input type="submit" value="swagger" />
+        </form>
+        <form action="/api-docs-json" method="GET">
+          <input type="submit" value="swagger json 링크" />
+        </form>
+
         <form action="/auth/google/menu" method="GET">
           <input type="submit" value="구글 로그인 메뉴" />
         </form>
         <form action="/auth/kakao/menu" method="GET">
           <input type="submit" value="카카오 로그인 메뉴" />
         </form>
+        
     `;
   }
 }
