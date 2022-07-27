a6f0f352f055b9b80893fa6440aa0b269f5a489a
jobum97 <brian654321@naver.com>
2022-07-27 15:38:13 +0900

    [#13]feat: cors, helmet 설정

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,36 @@ import { AppModule } from './app.module';
 import { SharedModule } from './shared/shared.module';
 import { ApiConfigService } from './shared/services/api-config.service';
 import { setupSwagger } from './utils/swagger';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const whitelist = [
+    'https://withyou-be.mayleaf.dev',
+    'http://localhost:3030',
+    'https://with-you-front-end.vercel.app',
+  ];
+  app.enableCors({
+    //origin: '*',
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin)! == -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
+
+  //app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
   const configService = app.select(SharedModule).get(ApiConfigService);
   const port = configService.appConfig.port;
   console.log('server run on port #', port);
