439bb6a9981321420a39614eb75fc17a053b8ebb
jobum97 <brian654321@naver.com>
2022-08-10 14:55:54 +0900

    [#16]fix: isNew description fix

--- src/auth/auth.DTO/jwtAccessTokenResponse.ts
+++ src/auth/auth.DTO/jwtAccessTokenResponse.ts
@@ -4,6 +4,6 @@ export class JwtAccessTokenResponse {
   @ApiProperty({ description: 'jwtToken' })
   accessToken: string;
 
-  @ApiProperty({ description: 'jwtToken' })
+  @ApiProperty({ description: '새로운 유저인지 판별하는 값' })
   isNew: boolean;
 }
