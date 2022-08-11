78c466e20b35a52788d85f6310ee84c61ea06a80
jobum97 <brian654321@naver.com>
2022-08-11 15:47:44 +0900

    [#16]fix: jwt 관련 테스트코드 제거

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,9 @@
 import { Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { JwtPayload } from './jwt/jwt.payload.interface';
-import JwtAuthGuard from './jwt/jwt.auth.guard';
 
 @Controller('auth')
 @ApiTags('인증 API')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Get('/get-jwt-token')
-  async login(@Query() query: JwtPayload): Promise<string> {
-    return await this.authService.getJwtToken(query);
-  }
-
-  @Post('/test-signin')
-  @UseGuards(JwtAuthGuard)
-  signin(@Req() req) {
-    return req.user;
-  }
 }
