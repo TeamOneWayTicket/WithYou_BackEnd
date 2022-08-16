c829597d5ff22bdf355bfc2ea7c41e617e8a6423
jobum97 <brian654321@naver.com>
2022-08-16 13:00:06 +0900

    [#20]fix: user 쪽 swagger 수정 및 안쓰는 코드정리

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -14,15 +14,10 @@ import { UserService } from './user.service';
 import { User } from './user.entity';
 import { UpdateUserDto } from './userDto/updateUserDto';
 import { CreateUserDto } from './userDto/createUserDto';
-import {
-  ApiOkResponse,
-  ApiOperation,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { BaseUsersResponse } from './userDto/baseUsersResponse';
 import { BaseUserResponse } from './userDto/baseUserResponse';
 import { DeleteUserResponse } from './userDto/deleteUserResponse';
-import JwtAuthGuard from '../auth/jwt/jwt.auth.guard';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
@@ -35,7 +30,7 @@ export class UserController {
   @Get('list')
   @ApiOkResponse({ description: '성공', type: BaseUsersResponse })
   @ApiOperation({
-    summary: '전체 유저 리스트 조회 API',
+    summary: 'getAllUserList',
     description: '전체 유저 리스트 받아온다.',
   })
   async findAll(): Promise<User[]> {
@@ -46,20 +41,17 @@ export class UserController {
   @Get('/login/:id')
   @ApiOkResponse({ description: '성공', type: BaseUserResponse })
   @ApiOperation({
-    summary: '특정 유저 로그인 처리 API',
+    summary: 'getUserById through jwt',
     description: '특정 id 유저 가지고 온다.',
   })
-  async login(
-    @Param('id', ParseIntPipe) id: number,
-    @Res() res,
-  ): Promise<User> {
+  async login(@Param('id', ParseIntPipe) id: number): Promise<User> {
     return await this.userService.findOne(id);
   }
 
   @Get(':id')
   @ApiOkResponse({ description: '성공', type: BaseUserResponse })
   @ApiOperation({
-    summary: '특정 유저 가지고 오는 API',
+    summary: 'getUserById',
     description: '특정 id 유저 가지고 온다.',
   })
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<User> {
@@ -69,7 +61,7 @@ export class UserController {
   @Get('localuser/:id')
   @ApiOkResponse({ description: '성공', type: BaseUserResponse })
   @ApiOperation({
-    summary: '특정 로컬유저 가지고 오는 API(Test 용도)',
+    summary: 'getLocalUserById (Test 용도)',
   })
   async findLocalUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
     return await this.userService.findLocalUser(id);
@@ -78,7 +70,7 @@ export class UserController {
   @Patch(':id')
   @ApiOkResponse({ description: '성공', type: BaseUserResponse })
   @ApiOperation({
-    summary: '특정 유저 정보 수정하는 API',
+    summary: 'updateUser',
     description: '특정 id 유저 정보를 수정한다.',
   })
   async updateUser(
@@ -91,7 +83,7 @@ export class UserController {
   @Post()
   @ApiOkResponse({ description: '성공', type: BaseUserResponse })
   @ApiOperation({
-    summary: '유저 정보 받아서 유저 생성하는 API',
+    summary: 'createUser',
     description: '유저 생성한다.',
   })
   async createUser(@Body() user: CreateUserDto): Promise<User> {
@@ -100,7 +92,7 @@ export class UserController {
 
   @Delete(':id')
   @ApiOperation({
-    summary: '유저 id 받아서 유저 삭제하는 API',
+    summary: 'deleteUser',
     description: '유저 삭제한다.',
   })
   @ApiOkResponse({ description: '성공', type: DeleteUserResponse })
