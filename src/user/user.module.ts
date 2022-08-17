71a554d4ef4e2065115a35ed8b6e485f7e50e7cf
Mayleaf <uh.jeong651@gmail.com>
2022-08-17 13:23:05 +0900

    [ON-4] feat: add UserPushToken to typeorm feature

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,9 +6,18 @@ import { UserController } from './user.controller';
 import { LocalUser } from './local.user.entity';
 import { KakaoUser } from './kakao.user.entity';
 import { GoogleUser } from './google.user.entity';
+import { UserPushToken } from './entity/user-push-token.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, LocalUser, KakaoUser, GoogleUser])],
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      LocalUser,
+      KakaoUser,
+      GoogleUser,
+      UserPushToken,
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
