249345b7f7bf106a3ef69628617556d6614d236f
Mayleaf <uh.jeong651@gmail.com>
2022-08-19 14:51:16 +0900

    [ON-21] feat: use validation with user properties

--- src/module/auth/dto/jwt-validation.dto.ts
+++ src/module/auth/dto/jwt-validation.dto.ts
@@ -3,16 +3,16 @@ import { IsString } from 'class-validator';
 
 export class JwtValidationDto {
   @ApiProperty({ description: '유저 타입' })
-  userType: string;
+  vendor: string;
 
   @ApiProperty({ description: '유저 id' })
-  userId: number;
+  id: number;
 
   @ApiProperty({ description: '유저 이름' })
-  userName: string;
+  nickname: string;
 
   @ApiProperty({ description: '유저 프로필 사진' })
-  userProfile: number;
+  thumbnail: string;
 
   @ApiProperty({ description: '새로운 유저인지 판별하는 값' })
   @IsString()
