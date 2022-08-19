376caa1a62840159f1efcc5ba7792c488308c364
jobum97 <brian654321@naver.com>
2022-08-19 13:26:12 +0900

    hotfix: 변수명 수정

--- src/diary/diary.module.ts
+++ src/diary/diary.module.ts
@@ -3,14 +3,14 @@ import { DiaryService } from './diary.service';
 import { DiaryController } from './diary.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Diary } from './diary.entity';
-import { DiaryMedia } from './diary.medium.entity';
+import { DiaryMedium } from './diary.medium.entity';
 import { AuthModule } from '../auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Diary, DiaryMedia]),
+    TypeOrmModule.forFeature([Diary, DiaryMedium]),
     AuthModule,
     JwtModule,
     UserModule,
