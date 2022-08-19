1c929e2ed0380d3c7724c03628ca2efa06fdba76
Mayleaf <uh.jeong651@gmail.com>
2022-08-19 14:01:49 +0900

    [ON-21] remove unused app.service.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get, Header } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get('')
   @Header('Content-Type', 'text/html')
   @ApiOperation({
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './module/user/user.module';
-import { DataSource } from 'typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { SharedModule } from './shared/shared.module';
 import { ApiConfigService } from './shared/services/api-config.service';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './module/auth/auth.module';
 import { DiaryModule } from './module/diary/diary.module';
 import { RedisModule } from '@liaoliaots/nestjs-redis';
@@ -38,8 +36,5 @@ import { FamilyModule } from './module/family/family.module';
     FamilyModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {
-  constructor(private readonly dataSource: DataSource) {}
-}
+export class AppModule {}
