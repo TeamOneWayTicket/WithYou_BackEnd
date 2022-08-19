b0518d21c3b02dd4b663b5ccebb336df72648264
Mayleaf <uh.jeong651@gmail.com>
2022-08-19 15:38:30 +0900

    [ON-21] fix: use array type for swagger apiproperty

--- src/module/diary/dto/create-media-response.dto.ts
+++ src/module/diary/dto/create-media-response.dto.ts
@@ -2,6 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { DiaryMedium } from '../entity/diary.medium.entity';
 
 export class CreateMediaResponseDto {
-  @ApiProperty({ description: '일기' })
+  @ApiProperty({ description: '일기', type: [DiaryMedium] })
   diaryMedia: DiaryMedium[];
 }
