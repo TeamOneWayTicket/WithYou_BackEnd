848fea6f319e6914ed9c8f98fa4e8ffc88c24369
jobum97 <brian654321@naver.com>
2022-08-22 15:26:21 +0900

    [#28]fix: change dto column name for correct meaning

--- src/module/diary/dto/put-presigned-urls.dto.ts
+++ src/module/diary/dto/put-presigned-urls.dto.ts
@@ -2,9 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { MediumInfoDto } from './medium-info.dto';
 
 export class PutPresignedUrlsDto {
-  @ApiProperty({ description: 'diaryId', example: 1 })
-  diaryId: number;
-
   @ApiProperty({
     description: 'media type & media quantity',
     example: [
