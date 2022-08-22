616f75f8572bdd8b458bc531c24d8ee012c11a6a
jobum97 <brian654321@naver.com>
2022-08-22 17:47:30 +0900

    [#28]feat: ValidationPipe option enable ImplicitConversion: true

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,9 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
 
