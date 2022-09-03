69b284b69d8d887e8279516694c1ec3e16ce157e
jobum97 <brian654321@naver.com>
2022-09-03 20:44:43 +0900

    [ON-44][#37]feat: create getInviteCode feature

--- src/module/family/family.service.ts
+++ src/module/family/family.service.ts
@@ -3,12 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Family } from './entity/family.entity';
 import { CreateFamilyDto } from './dto/create-family.dto';
+import { FamilyInviteCode } from './entity/family.invite.code.entity';
+import { FamilyInviteCodeDto } from './dto/family-invite-code.dto';
+import crypto from 'crypto';
 
 @Injectable()
 export class FamilyService {
   constructor(
     @InjectRepository(Family)
     private familyRepository: Repository<Family>,
+    @InjectRepository(FamilyInviteCode)
+    private familyInviteCodeRepository: Repository<FamilyInviteCode>,
   ) {}
 
   async createFamily(dto: CreateFamilyDto): Promise<Family> {
@@ -21,5 +26,15 @@ export class FamilyService {
 
   async deleteFamily(id: number): Promise<void> {
     await this.familyRepository.delete({ id });
+  }
+
+  async getInviteCode(familyId: number): Promise<FamilyInviteCodeDto> {
+    const inviteCode = crypto.randomBytes(5).toString('hex');
+    await this.familyInviteCodeRepository.save({
+      familyId,
+      inviteCode,
+    });
+
+    return { inviteCode };
   }
 }
