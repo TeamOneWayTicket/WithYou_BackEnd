96730bf9ac76dffe8aa500a40df5e9ef8096964d
jobum97 <brian654321@naver.com>
2022-09-03 20:44:08 +0900

    [ON-44][#37]feat: FamilyInviteCode entity at family.module.ts

--- src/module/family/family.module.ts
+++ src/module/family/family.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Family } from './entity/family.entity';
 import { FamilyController } from './family.controller';
 import { UserModule } from '../user/user.module';
+import { FamilyInviteCode } from './entity/family.invite.code.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Family]), UserModule],
+  imports: [TypeOrmModule.forFeature([Family, FamilyInviteCode]), UserModule],
   controllers: [FamilyController],
   providers: [FamilyService],
   exports: [FamilyService],
