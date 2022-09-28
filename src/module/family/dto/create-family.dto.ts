d75018193b207e55f6deb313bac9a0eb96b4251a
jobum97 <brian654321@naver.com>
2022-09-28 18:34:52 +0900

    [ON-71][#42]fix: bug fix

--- src/module/family/dto/create-family.dto.ts
+++ src/module/family/dto/create-family.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateFamilyDto {
   @ApiProperty({ description: '가족 이름' })
+  @IsString()
   name: string;
 }
