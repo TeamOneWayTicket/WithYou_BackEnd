2cb17ed6409c3502b07965259997516b31860430
jobum97 <brian654321@naver.com>
2022-09-29 21:03:19 +0900

    [ON-76][#51]fix: fix DiaryMedium Entity  -add createdAt

--- src/module/diary/entity/diary.medium.entity.ts
+++ src/module/diary/entity/diary.medium.entity.ts
@@ -9,6 +9,8 @@ import {
 import { ApiExtraModels } from '@nestjs/swagger';
 import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 import { Diary } from './diary.entity';
+import { LocalDatetimeTransformer } from '../../../transformer/local-datetime.transformer';
+import { LocalDateTime } from '@js-joda/core';
 
 @Entity()
 @ApiExtraModels()
@@ -37,4 +39,13 @@ export class DiaryMedium {
   })
   @JoinColumn({ name: 'diary_id' })
   diary: Diary;
+
+  @ApiModelProperty({ description: 'medium 작성 시점', type: Date })
+  @Index()
+  @Column({
+    type: 'timestamp without time zone',
+    default: () => 'CURRENT_TIMESTAMP',
+    transformer: new LocalDatetimeTransformer(),
+  })
+  createdAt: LocalDateTime;
 }
