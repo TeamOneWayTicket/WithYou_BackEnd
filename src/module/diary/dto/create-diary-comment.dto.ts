73c4f8a693c3d70e00f0a3596da4903d1a5b535d
jobum97 <brian654321@naver.com>
2022-09-30 15:14:10 +0900

    [ON-76][#51]fix: create-diary-comment.dto.ts

--- src/module/diary/dto/create-diary-comment.dto.ts
+++ src/module/diary/dto/create-diary-comment.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateDiaryCommentDto {
   @ApiProperty({ description: '가족 id', example: 'createComment test' })
+  @IsNumber()
   diaryId: number;
 
   @ApiProperty({ description: '댓글 내용', example: 'createComment test' })
+  @IsString()
   content: string;
 }
