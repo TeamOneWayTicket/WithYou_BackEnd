ce8e493850bfd7b5390f660732b304441a67870e
jobum97 <brian654321@naver.com>
2022-09-30 14:12:30 +0900

    [ON-76][#51]feat: add methods in diary.comment.service.ts

--- src/module/diary/service/diary.comment.service.ts
+++ src/module/diary/service/diary.comment.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { DiaryComment } from '../entity/diary.comment.entity';
+import { DiaryCommentsDto } from '../dto/diary-comments.dto';
+import { CreateDiaryCommentDto } from '../dto/create-diary-comment.dto';
 
 @Injectable()
 export class DiaryService {
@@ -9,4 +11,14 @@ export class DiaryService {
     @InjectRepository(DiaryComment)
     private readonly diaryCommentRepository: Repository<DiaryComment>,
   ) {}
+
+  async findAllComments(diaryId: number): Promise<DiaryCommentsDto> {
+    return {
+      comments: await this.diaryCommentRepository.find({ where: { diaryId } }),
+    };
+  }
+
+  async createComment(dto: CreateDiaryCommentDto): Promise<DiaryComment> {
+    return await this.diaryCommentRepository.save({ ...dto });
+  }
 }
