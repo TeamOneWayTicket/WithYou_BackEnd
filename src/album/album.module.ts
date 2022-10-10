07138ad51885a76047747cdd9f9e0950d73d3fbb
jobum97 <brian654321@naver.com>
2022-10-10 17:27:17 +0900

    [ON-96][#64]feat: add getFamilyPhotos

--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AlbumController } from './album.controller';
 import { AlbumService } from './album.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DiaryMedium } from '../module/diary/entity/diary.medium.entity';
+import { Diary } from '../module/diary/entity/diary.entity';
+import { DiaryModule } from '../module/diary/diary.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Diary, DiaryMedium]), DiaryModule],
   controllers: [AlbumController],
   providers: [AlbumService],
 })
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MediaDto } from './dto/media.dto';
+import { DiaryMedium } from '../module/diary/entity/diary.medium.entity';
+import { Diary } from '../module/diary/entity/diary.entity';
 
 @Injectable()
-export class AlbumService {}
+export class AlbumService {
+  constructor(
+    @InjectRepository(DiaryMedium)
+    private diaryMediumRepository: Repository<DiaryMedium>,
+    @InjectRepository(Diary)
+    private diaryRepository: Repository<Diary>,
+  ) {}
+
+  async getFamilyPhotos(familyId: number): Promise<MediaDto> {
+    const diaries = await this.diaryRepository.find({ where: { familyId } });
+    const media: DiaryMedium[] = [];
+    for (const diary of diaries) {
+      for (const diaryMedium of diary.media) {
+        media.push(diaryMedium);
+      }
+    }
+    return { media };
+  }
+}
