43651273a9592c44e0c6a86b6687f80c7f16d2e3
jobum97 <brian654321@naver.com>
2022-10-12 16:26:49 +0900

    [ON-96][#64]fix: change media.dto.ts => album.media.dto.ts, album.medium.dto.ts

--- src/module/album/dto/media.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { DiaryMedium } from '../../diary/entity/diary.medium.entity';
-
-export class MediaDto {
-  @ApiProperty({ description: '사진/동영상' })
-  media: DiaryMedium[];
-}
