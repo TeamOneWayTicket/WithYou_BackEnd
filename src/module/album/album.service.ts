97c66f209a3b0a76a5b98d6ce1771cd5afa55162
jobum97 <brian654321@naver.com>
2022-10-18 21:17:41 +0900

    [ON-106][#44]fix: get cf url by using getUrl method in album.service.ts

--- src/module/album/album.service.ts
+++ src/module/album/album.service.ts
@@ -4,9 +4,9 @@ import { Repository } from 'typeorm';
 import { AlbumMediaDto } from './dto/album.media.dto';
 import { DiaryMedium } from '../diary/entity/diary.medium.entity';
 import { Diary } from '../diary/entity/diary.entity';
-import AWS from 'aws-sdk';
 import { ApiConfigService } from '../../shared/services/api-config.service';
 import _ from 'lodash';
+import { getUrl } from '../../transformer/url.transformer';
 
 @Injectable()
 export class AlbumService {
@@ -16,27 +16,20 @@ export class AlbumService {
     private readonly configService: ApiConfigService,
     @InjectRepository(Diary)
     private readonly diaryRepository: Repository<Diary>,
-  ) {
-    AWS.config.update({
-      region: this.configService.awsConfig.bucketRegion,
-      accessKeyId: this.configService.awsConfig.accessKey,
-      secretAccessKey: this.configService.awsConfig.secretAccessKey,
-    });
-  }
+  ) {}
 
   async getFamilyPhotos(familyId: number): Promise<AlbumMediaDto> {
     const diaries = await this.diaryRepository.find({
       where: { familyId },
       relations: ['media'],
     });
-    const cf = this.configService.awsConfig.cfAddress;
     const media = await Promise.all(
       _.flatMap(diaries, (item) => {
         return _(item.media)
           .map(async function (value) {
             return {
               diaryId: value.diaryId,
-              url: cf + value.fileNameInS3,
+              url: await getUrl(value.fileNameInS3, 200),
             };
           })
           .value();
