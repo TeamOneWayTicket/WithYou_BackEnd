08d6b328539215ba2eae14b5a91c7076d08a1218
jobum97 <brian654321@naver.com>
2022-10-25 14:06:13 +0900

    [ON-109][#73]feat: create local login

--- src/module/auth/controller/local.auth.controller.ts
+++ src/module/auth/controller/local.auth.controller.ts
@@ -1,18 +1,29 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtValidationDto } from '../dto/jwt-validation.dto';
-import { Auth } from '../../../decorator/http.decorator';
-import { Role } from '../../../common/enum/role.enum';
 import { User } from '../../user/entity/user.entity';
-import { UserParam } from '../../../decorator/user.decorator';
 import { JwtService } from '@nestjs/jwt';
 import { LocalUserDto } from '../../user/dto/local.user.dto';
 import { LocalAuthService } from '../service/local.auth.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LocalUser } from '../../user/entity/local.user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Controller('auth/local')
 @ApiTags('인증 API')
 export class LocalAuthController {
   constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(LocalUser)
+    private readonly localUserRepository: Repository<LocalUser>,
     private readonly localAuthService: LocalAuthService,
     private readonly jwtService: JwtService,
   ) {}
@@ -41,13 +52,36 @@ export class LocalAuthController {
   }
 
   @Post('/login')
-  @Auth(Role.User)
   @ApiOkResponse({ description: '성공', type: JwtValidationDto })
   @ApiOperation({
     summary: 'local login',
   })
-  async login(@UserParam() user: User): Promise<void> {
-    // 회원가입 한적없는데 로그인 시도시 badreqest
-    // 아니라면 jwt 발급 + 쿠키 세팅 쿠키 세팅 해줘야함
+  async login(@Body() dto: LocalUserDto, @Res() res): Promise<void> {
+    const localUser = await this.localUserRepository.findOne({
+      where: { email: dto.email },
+    });
+    if (!localUser) {
+      throw new BadRequestException(400, '일치하는 유저가 없습니다');
+    }
+    const isValid = bcrypt.compare(dto.password, localUser.password);
+    if (!isValid) {
+      throw new BadRequestException(400, '비밀번호가 틀렸습니다');
+    }
+    const user = await this.userRepository.findOne({
+      where: { id: localUser.userId },
+    });
+    const token = this.jwtService.sign({
+      id: user.id,
+      vendor: 'local',
+      nickname: user.nickname,
+      thumbnail: user.nickname,
+    });
+    res.cookie('jwt', token, {
+      domain: '.with-you.io',
+      httpOnly: false,
+      sameSite: 'strict',
+      secure: true,
+    });
+    return res.redirect(302, 'https://frontend.with-you.io/');
   }
 }
