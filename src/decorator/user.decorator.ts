b07efa62e43cbf86fc99c1eb775f40ffd717a3cc
jobum97 <brian654321@naver.com>
2022-10-31 15:22:42 +0900

    [ON-108][#72]remove: DateParam

--- src/decorator/user.decorator.ts
+++ src/decorator/user.decorator.ts
@@ -1,18 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { DateTimeUtil } from '../common/date-time-util';
 
 export const UserParam = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;
-  },
-);
-
-export const DateParam = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return {
-      date: DateTimeUtil.toLocalDateTimeBy(request.params.date + ' 00:00:00'),
-    };
   },
 );
