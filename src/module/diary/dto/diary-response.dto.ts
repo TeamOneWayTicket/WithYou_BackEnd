631fdb2baa6ba711351648de050bcb927c7b52a2
jobum97 <brian654321@naver.com>
2022-11-06 18:27:34 +0900

    [ON-113][#83]fix: add author in diary-response.dto.ts

--- src/module/diary/dto/diary-response.dto.ts
+++ src/module/diary/dto/diary-response.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Diary } from '../entity/diary.entity';
+import { User } from '../../user/entity/user.entity';
 
 export class DiaryResponseDto {
   @ApiProperty({ description: '일기' })
   diary: Diary;
+
+  @ApiProperty({ description: '일기' })
+  author: User;
 
   @ApiProperty({ description: '댓글 갯수' })
   commentCount: number;
